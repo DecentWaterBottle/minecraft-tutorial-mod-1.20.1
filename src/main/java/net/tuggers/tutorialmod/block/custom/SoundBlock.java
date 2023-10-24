f097bae6e287bf170cf0967d309ca5c8e1b3a02f
Jack <jack.carter334@gmail.com>
2023-10-24 11:53:26 +0200

    Add Sound Block item

---
+++ src/main/java/net/tuggers/tutorialmod/block/custom/SoundBlock.java
@@ -0,0 +1,40 @@
+package net.tuggers.tutorialmod.block.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import java.util.Random;
+
+public class SoundBlock extends Block {
+
+    public SoundBlock(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+
+
+        pLevel.playSound(pPlayer, pPos, SoundEvents.NOTE_BLOCK_DIDGERIDOO.get(), SoundSource.BLOCKS,
+                1f, 1f);
+
+        return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void fallOn(Level pLevel, BlockState pState, BlockPos pPos, Entity pEntity, float pFallDistance) {
+        Random rand = new Random();
+        pLevel.playSound(pEntity, pPos, SoundEvents.NOTE_BLOCK_DIDGERIDOO.get(), SoundSource.BLOCKS,
+                2f, rand.nextFloat() * (2 - 1) + 1);
+
+        super.fallOn(pLevel, pState, pPos, pEntity, pFallDistance);
+    }
+}
