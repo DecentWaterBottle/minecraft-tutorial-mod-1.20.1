ccbca042028c480edd227280f08d925e1c1c1f2a
Jack <jack.carter334@gmail.com>
2023-10-24 12:48:21 +0200

    Add tooltips for MetalDetector item and SoundBlock block.

--- src/main/java/net/tuggers/tutorialmod/block/custom/SoundBlock.java
+++ src/main/java/net/tuggers/tutorialmod/block/custom/SoundBlock.java
@@ -1,16 +1,23 @@
 package net.tuggers.tutorialmod.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 import java.util.Random;
 
 public class SoundBlock extends Block {
@@ -27,6 +34,12 @@ public class SoundBlock extends Block {
                 1f, 1f);
 
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable BlockGetter pLevel, List<Component> pTooltip, TooltipFlag pFlag) {
+        pTooltip.add(Component.literal("Makes sweet sounds when right clicked or jumped on"));
+        super.appendHoverText(pStack, pLevel, pTooltip, pFlag);
     }
 
     @Override
