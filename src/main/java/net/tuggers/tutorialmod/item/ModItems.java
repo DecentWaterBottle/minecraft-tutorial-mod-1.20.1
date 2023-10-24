321650e5a0597050d02fae71b00983495ef767b4
Jack <jack.carter334@gmail.com>
2023-10-24 17:36:11 +0200

    Add set of sapphire tools

--- src/main/java/net/tuggers/tutorialmod/item/ModItems.java
+++ src/main/java/net/tuggers/tutorialmod/item/ModItems.java
@@ -1,6 +1,6 @@
 package net.tuggers.tutorialmod.item;
 
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -27,6 +27,24 @@ public class ModItems {
 
     public static final RegistryObject<Item> PINECONE = ITEMS.register("pinecone",
             () -> new FuelItem(new Item.Properties(), 100));
+
+    public static final RegistryObject<Item> SAPPHIRE_SWORD = ITEMS.register("sapphire_sword",
+            () -> new SwordItem(ModToolTiers.SAPPHIRE, 4, 3, new Item.Properties()));
+
+    public static final RegistryObject<Item> SAPPHIRE_PICKAXE = ITEMS.register("sapphire_pickaxe",
+            () -> new PickaxeItem(ModToolTiers.SAPPHIRE, 2, 2, new Item.Properties()));
+
+    public static final RegistryObject<Item> SAPPHIRE_AXE = ITEMS.register("sapphire_axe",
+            () -> new AxeItem(ModToolTiers.SAPPHIRE, 3, 1, new Item.Properties()));
+
+    public static final RegistryObject<Item> SAPPHIRE_SHOVEL = ITEMS.register("sapphire_shovel",
+            () -> new ShovelItem(ModToolTiers.SAPPHIRE, 4, 2, new Item.Properties()));
+
+    public static final RegistryObject<Item> SAPPHIRE_HOE = ITEMS.register("sapphire_hoe",
+            () -> new HoeItem(ModToolTiers.SAPPHIRE, 1, 5, new Item.Properties()));
+
+    public static final RegistryObject<Item> SAPPHIRE_STAFF = ITEMS.register("sapphire_staff",
+            () -> new Item(new Item.Properties().stacksTo(1)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
